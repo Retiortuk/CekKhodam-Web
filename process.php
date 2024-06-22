b1db3108eced1ed6d91c69ce36eac49e22f89160
Retiortuk <aauliagazzam@gmail.com>
2024-06-22 21:15:05 +0700

    feat: database

--- process.php
+++ process.php
@@ -1,1 +1,15 @@
+<?php 
 
+function kodam_program(){
+
+    $data_kodam = ['Kak Gem', 'Macan Hitam', 'Macan Putih', 'Aing Maung', 'Singa', 'Gajah', 'Tukang Parkir
+    ', 'Burung Dara', 'Pak Vinsen', 'Tidak Memiliki Khodam', 'Daffa Ariq', 'Genderuwo', 'Sosok Hitam Tinggi Kiris'] ;
+
+    $kepribadian_kodam = [
+        'Kak Gem'=>'Ikan Teri Ikan Tongkol, PAHAM!!',
+        'Macan Hitam'=>'Seseorang Yang Sangar',
+        'Macan Putih'=>'Cenderung Memiliki Hati Dan Sifat Yang Bersih Dompet Anda Pun Bersih',
+        ''
+    ];
+}
+?>
