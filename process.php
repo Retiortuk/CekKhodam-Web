53b4e6f3c15a7d4006a50a7dcf2df735cf51ee9e
Retiortuk <aauliagazzam@gmail.com>
2024-06-23 12:12:55 +0700

    final

--- process.php
+++ process.php
@@ -1,137 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cek Hasil Khodam</title>
-</head>
-
-
-<body>
-
-    <?php 
-        /* function kodam_program($nama, $umur){
-        // DATABASE
-        $data_kodam = ['Kak Gem', 'Macan Hitam', 'Macan Putih', 'Aing Maung', 'Singa', 'Gajah', 'Tukang Parkir
-        ', 'Burung Dara', 'Pak Vinsen', 'Tidak Memiliki Khodam', 'Daffa Ariq', 'Genderuwo', 'Sosok Hitam Tinggi Kiris'] ;
-
-        $kepribadian_kodam = [
-            'Kak Gem'=>'Ikan Teri Ikan Tongkol, PAHAM!!',
-            'Macan Hitam'=>'Seseorang Yang Sangar',
-            'Macan Putih'=>'Cenderung Memiliki Hati Dan Sifat Yang Bersih Dompet Anda Pun Bersih',
-            'Aing Maung'=>'Kamu Memiliki Sifat Yang Mudah Marah Emosian',
-            'Singa'=>'Cenderung Memiliki sifat yang Pemberani dan Rajin',
-            'Gajah'=>'Memiliki Hati Yang Besar',
-            'Tukang Parkir'=>'Memiliki Sifat Suka Mengatur Dan Suka Muncul Tiba-Tiba',
-            'Burung Dara'=>'Seseorang Yang Baik Hati dan Setia',
-            'Pak Vinsen'=>'Suka Minta Nasi 2 Sama ***** Satu',
-            'Tidak Memiliki Khodam'=>'Orang Yang Bersih Dan Suci Agamis',
-            'Daffa Ariq'=>'Bahaya Sebentar Lagi Kamu Berubah Menjadi Anomali',
-            'Genderuwo'=>'Sosok Tunggu Kiris',
-            'Sosok Hitam Tinggi Kiris'=>'Seseorang Yang Misterius',
-        ];
-
-        // OUTPUT
-        $hasil_khodam = $data_kodam[array_rand($data_kodam)];
-        $result = "<p>Halo <strong>$nama</strong> Ternyata Sosok Yang menetap Di Diri Anda <strong>$hasil_khodam</strong></p>";
-        if(array_key_exists($hasil_khodam, $kepribadian_kodam)){
-            $result.= "<p>Kepribadian $hasil_khodam Artinya ". $kepribadian_kodam[$data_kodam] . "</p>";
-        }
-        return $result;
-    }
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nama = $_POST['nama'];
-        $umur = $_POST['umur'];
-        if (preg_match('/^[a-zA-Z\s-]+$/', $nama) && ctype_digit($umur)) {
-            $umur = (int) $umur;
-            echo kodam_program($nama, $umur);
-        } else {
-            echo "<p>Masukkan Nama dan Umur yang valid!</p>";
-        }
-    } */
-    ?>
-
-
-    
-</body>
-</html>
-
-
-
-
-
- -->
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <?php
-    /* function khodam_program($nama, $umur) {
-        // DATABASE
-        $data_khodam = ['Kak Gem', 'Macan Hitam', 'Macan Putih', 'Aing Maung', 'Singa', 'Gajah', 'Tukang Parkir
-        ', 'Burung Dara', 'Pak Vinsen', 'Tidak Memiliki Khodam', 'Daffa Ariq', 'Genderuwo', 'Sosok Hitam Tinggi Kiris'];
-
-        $kepribadian_khodam = [
-            'Kak Gem'=>'Ikan Teri Ikan Tongkol, PAHAM!!',
-            'Macan Hitam'=>'Seseorang Yang Sangar',
-            'Macan Putih'=>'Cenderung Memiliki Hati Dan Sifat Yang Bersih Dompet Anda Pun Bersih',
-            'Aing Maung'=>'Kamu Memiliki Sifat Yang Mudah Marah Emosian',
-            'Singa'=>'Cenderung Memiliki sifat yang Pemberani dan Rajin',
-            'Gajah'=>'Memiliki Hati Yang Besar',
-            'Tukang Parkir'=>'Memiliki Sifat Suka Mengatur Dan Suka Muncul Tiba-Tiba',
-            'Burung Dara'=>'Seseorang Yang Baik Hati dan Setia',
-            'Pak Vinsen'=>'Suka Minta Nasi 2 Sama ***** Satu',
-            'Tidak Memiliki Khodam'=>'Orang Yang Bersih Dan Suci Agamis',
-            'Daffa Ariq'=>'Bahaya Sebentar Lagi Kamu Berubah Menjadi Anomali',
-            'Genderuwo'=>'Sosok Tunggu Kiris',
-            'Sosok Hitam Tinggi Kiris'=>'Seseorang Yang Misterius',
-        ];
-
-        // OUTPUT
-        $hasil_khodam = $data_khodam[array_rand($data_khodam)];
-        $result = "Halo $nama, Khodam Kamu $hasil_khodam";
-        if (array_key_exists($hasil_khodam, $kepribadian_khodam)) {
-            $result .= "Kepribadian $hasil_khodam Artinya $kepribadian_khodam[$hasil_khodam]" ;
-        }
-        return $result;
-    }
-
-    $result = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nama = $_POST['nama'];
-        $umur = $_POST['umur'];
-        if (preg_match('/^[a-zA-Z\s-]+$/', $nama) && ctype_digit($umur)) {
-            $umur = (int) $umur;
-            echo khodam_program($nama, $umur);
-        } else {
-            echo "<p>Masukkan Nama dan Umur yang valid!</p>";
-        }
-    } */
-    ?>
-
-    <div class="container_hasil">
-        <h1>Hasil Khodam</h1>
-        <div class="hasil">
-            <p> Test</p>
-        </div>
-    </div>
-
-    <form action="index.html" method="get">
-        <input type="submit" value="Cek Lagi">
-    </form>
-
-</body>
-</html> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -212,10 +78,22 @@
         }
 
         .by {
-            color: rgb(129, 129, 129);
+            color: rgb(221, 221, 221);
             font-size: 11px;
             padding: 13px;
             font-family: 'Poppins', sans-serif;
+        }
+
+        .a{
+            color: rgb(180, 180, 180);
+		    font-size: 11px;
+            text-decoration: none;
+        }
+
+        .a:hover{
+            color: inherit;
+            text-decoration: underline;
+            text-decoration: none
         }
     </style>
 </head>
@@ -272,7 +150,7 @@
         </div>
         <a href="index.html" class="btn">Cek Lagi</a>
         <div class="by">
-            <i>By GitHub: Retiortuk ~2024~</i>
+        <a href="https://github.com/Retiortuk" class="a" target="_blank"><i>By GitHub: Retiortuk ~2024~</i></a>
         </div>
     </div>
 
