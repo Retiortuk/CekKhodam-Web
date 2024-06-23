9236a811ef5c2ec3cd49894d95bb9f320012fa7c
Retiortuk <aauliagazzam@gmail.com>
2024-06-23 17:27:10 +0700

    fix: mobile preview

--- process.php
+++ process.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -30,7 +30,7 @@
             max-width: 500px;
             width: 100%; */
             background-color: rgb(27, 36, 53);
-            height: 365px;
+            height: 450px;
             width: 540px;
             padding: 10px;
             position: absolute;
@@ -42,6 +42,7 @@
             text-align: center;
             border-radius: 15px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 90vw;
        }
 
         h1 {
